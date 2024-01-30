d6bf4fd504ec26f5db4c3fc0f72fdc036b4a6189
frederico <fredyramos0707@gmail.com>
2024-01-30 13:10:10 -0300

    91 - criando pagina signup

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,10 +1,47 @@
 import React from 'react-native';
 import {View, Text} from 'react-native';
 
+import { 
+    Background, 
+    Container, 
+    AreaInput, 
+    Input, 
+    SubmitButton, 
+    SubmitText
+  } from '../Signin/styles';
+
 export default function SignUp() {
     return(
-        <View>
-            <Text>Tela Cadastro</Text>
-        </View>
+        <Background>
+        <Container
+          behavior={Platform.OS === 'ios' ? 'padding' : ''}
+          enabled
+        >
+  
+          <AreaInput>
+            <Input
+              placeholder="Nome"
+            />
+          </AreaInput>
+  
+          <AreaInput>
+            <Input
+              placeholder="Seu email"
+            />
+          </AreaInput>
+  
+          <AreaInput>
+            <Input
+              placeholder="Sua senha"
+            />
+          </AreaInput>
+  
+          <SubmitButton>
+            <SubmitText>Cadastrar</SubmitText>
+          </SubmitButton>
+  
+        </Container>
+  
+      </Background>
     )
 }
