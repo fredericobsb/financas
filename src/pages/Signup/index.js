7412eb7a533a9c8d872551a0fecb4169e39ea6c1
frederico <fredyramos0707@gmail.com>
2024-01-30 11:54:47 -0300

    commit inicial

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,7 @@
 import React from 'react-native';
 import {View, Text} from 'react-native';
 
-export default function SignUp(){
+export default function SignUp() {
     return(
         <View>
             <Text>Tela Cadastro</Text>
