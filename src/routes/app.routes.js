46c2a472000649ff691f3d6f2f6c87483dda9010
frederico <fredyramos0707@gmail.com>
2024-02-03 19:36:42 -0300

    104 - Componente tipo de registro

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,18 +2,39 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../pages/Home';
+import New from '../pages/New'
 
 const AppDrawer = createDrawerNavigator();
 
 function AppRoutes(){
     return(
-        <AppDrawer.Navigator>
-            <AppDrawer.Screen 
-                name="Home"
-                component={Home}>
+        <AppDrawer.Navigator
+        screenOptions={{
+          headerShown:false,
+  
+          drawerStyle:{
+            backgroundColor: '#FFF',
+            paddingTop: 20,
+          },
+  
+          drawerActiveBackgroundColor:'#3b3dbf',
+          drawerActiveTintColor: '#FFF',
+  
+          drawerInactiveBackgroundColor: '#F0F2FF',
+          drawerInactiveTintColor: '#121212'
+  
+        }}
+      >
+        <AppDrawer.Screen
+          name="Home"
+          component={Home}
+        />
 
-            </AppDrawer.Screen>
-        </AppDrawer.Navigator>
+        <AppDrawer.Screen
+            name="Registrar"
+            component={New}
+        />
+      </AppDrawer.Navigator>
     )
 }
 
